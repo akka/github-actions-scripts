5814c4f8a746d60e9b995c93bb38d2323713e299
Enno Runne <458526+ennru@users.noreply.github.com>
2025-11-25 11:42:17 +0100

    Centralise pom.xml diff detection (#5)

---
+++ pom-no-diff.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# ensure the build didn't change any git controlled files
+set -euo pipefail
+
+if git diff --exit-code --quiet; then
+  echo "✅ No changes detected."
+else
+  echo "❌ ERROR: Generated pom.xml files are out of sync with the repository." >&2
+  echo "Run 'sbt makePom' and https://github.com/akka/github-actions-scripts/raw/refs/heads/main/pom-organize.sh in the repo directory to update and commit the resulting changes." >&2
+  git diff
+  exit 1
+fi
